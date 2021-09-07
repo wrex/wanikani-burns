fa78536f42a35fcb93390ce55fd7613138561d2f
Rex Walters <rw@pobox.com>
2021-09-07 11:16:49 -0700

    initial version of burn-progress.user.js

---
+++ burn-progress.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.wanikani.com/dashboard
+// @icon         https://www.google.com/s2/favicons?domain=wanikani.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
